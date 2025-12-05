80325f71dd615722939843694a2daecb01b384a7
Mariam236240 <karemalotfy7uuo@gmail.com>
2025-12-05 16:31:35 +0200

    Refactor routing and add padding in App component
    Updated route paths and added padding to prevent overlap with fixed navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import About from "./components/about";
-import Contact from "./components/Contact"
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/index-bbfl2k4X.css";
+
 export default function App() {
   return (
     <HashRouter>
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/about" element={<About />} />
-      
-<Route path="/contact" element={<Contact />} />
-<Route path="/Portfolio" element={<Portfolio />} />
-      </Routes>
+      {/* Add padding to prevent overlap with fixed navbar */}
+      <div style={{ paddingTop: "130px" }}>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
 
       <Footer />
     </HashRouter>
